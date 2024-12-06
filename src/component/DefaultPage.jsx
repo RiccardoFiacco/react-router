c25d3571aa15b0221b21ea897a5b45a36c02faef
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 15:49:41 +0100

    changes on defaultPage and add navigation

--- src/component/DefaultPage.jsx
+++ src/component/DefaultPage.jsx
@@ -1,10 +1,13 @@
-import { Header } from "./components/header/Header";
-import { Footer } from "./components/footer/Footer";
+import {Header}  from "./header/Header";
+import {Footer}  from "./footer/Footer";
+import { Navigation } from "./navbar/Navigation";
+import style from "./DefaultPage.module.css";
 export function DefaultPage(){
     return(
-        <>
+        <div className={`d-flex flex-column ${style.vh100}`}>
             <Header/>
+            <Navigation/>
             <Footer/>
-        </>
+        </div>
     )
 }
---
+++ src/component/navbar/Navigation.jsx
@@ -0,0 +1,15 @@
+export function Navigation(){
+    return(
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <a className="nav-link" href="#">Features</a>
+                    <a className="nav-link" href="#">Pricing</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
