15bdcbb57b46851d17fe58eef87de1c6235f5314
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 17:22:31 +0100

    add chi siamo

---
+++ src/component/Main/chisiamo/ChiSiamo.jsx
@@ -0,0 +1,12 @@
+
+export function ChiSiamo(){
+    return (
+        <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div className="col">
+                    <p>chi siamo?</p>
+                </div>
+            </div>     
+        </div>
+    )
+}
