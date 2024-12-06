e390bc74aba0e07d9e258c26f6b7b7259603fbb8
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 13:22:46 +0100

    done base router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
 
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultPageLayout}>
+          <Route path='/' Component={DefaultPageLayout}></Route>
+          <Route path='/' Component={DefaultPageLayout}></Route>
+          <Route path='/' Component={DefaultPageLayout}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
