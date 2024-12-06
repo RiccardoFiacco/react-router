eab0e95009c38687ba3564e2d92a38c0a1d48c3e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 16:08:13 +0100

    create home

---
+++ src/component/Main/home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+export function Home(){
+    return(
+        <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div className="col">
+                    <p>ciao sono la home</p>
+                </div>
+            </div>     
+        </div>        
+    )
+}
