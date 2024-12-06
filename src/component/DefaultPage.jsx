8fd7f085087dab206956fb0e9355bc2fccd635e6
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 15:36:18 +0100

    create the default page

---
+++ src/component/DefaultPage.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
+export function DefaultPage(){
+    return(
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
