ce469bde3e51b5d3eb0e844ddf9f4c82aeb4f8a5
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-06 17:23:14 +0100

    add all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import {DefaultPage} from './component/DefaultPage'
 import { Home } from './component/Main/home/Home'
+import { ChiSiamo } from './component/Main/chisiamo/ChiSiamo'
+import { Posts } from './component/Main/posts/Posts'
 function App() {
 
 
@@ -10,9 +12,8 @@ function App() {
       <Routes>
         <Route Component={DefaultPage}>
           <Route path='/' Component={Home}></Route>
-          {/* 
-          <Route path='/' Component={}></Route>
-          <Route path='/' Component={}></Route> */}
+          <Route path='/chi_siamo' Component={ChiSiamo}></Route>
+           <Route path='/posts' Component={Posts}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
