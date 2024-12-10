b154524b333c7a9ffeaaab4d271282c234bc03e2
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-10 15:28:58 +0100

    add not found page

---
+++ src/component/Main/not found/notFound.jsx
@@ -0,0 +1,12 @@
+export function notFound(){
+    return(
+        <div className="container">
+            <div className="row text-center pt-5">
+                <div className="col">
+                    <h1>pagina non trovata</h1>
+                    <h3>404</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
